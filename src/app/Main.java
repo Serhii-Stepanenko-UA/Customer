3b15c4726d8f18b99471187669e351aaa00416ef
Pulsar UA <sergivstep@gmail.com>
2024-06-15 20:16:17 +0300

    modify output

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer("Tom", "555 123-8596");
-        String output = "Customer: " + ", phone ";
+        String output = "Customer: " + customer.getName() + ", phone " + customer.getPhone();
         System.out.println(output);
     }
 }
