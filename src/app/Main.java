6e2ba2b85ee6b6961b2f93709a521e5ed708e7ba
Pulsar UA <sergivstep@gmail.com>
2024-06-15 20:12:44 +0300

    creat customer using constructor

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        Customer customer = new Customer("Tom", "555 123-8596");
         String output = "Customer: " + ", phone ";
         System.out.println(output);
     }
