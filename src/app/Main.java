d512ee8df7adf8c1e094be500060df4afaeac1a4
Pulsar UA <sergivstep@gmail.com>
2024-06-15 20:39:13 +0300

    modify creating customer

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer("Tom", "555 123-8596");
+        Customer customer = getCustomer(getData());
         String output = "Customer: " + customer.getName() + ", phone " + customer.getPhone();
         System.out.println(output);
     }
